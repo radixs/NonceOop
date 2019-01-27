78bfb260cafebffa051f18916f599e2091e4268f
Radoslaw Kurowski <radix@salvilines.eu>
2019-01-27 21:25:26 +0100

    Create NonceFacadeTest.php

---
+++ tests/src/NonceFacadeTest.php
@@ -0,0 +1,12 @@
+<?php
+use radix\NonceOop\NonceFacade;
+
+use PHPUnit\Framework\TestCase; //TODO
+
+class NonceFacadeTest extends TestCase {
+
+    public function testCreateSimpleNonce() {
+        // https://phpunit.de/getting-started/phpunit-7.html
+    }
+
+}
